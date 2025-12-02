70af041f24c7f71e4357c096f04c1077d22a93cb
zanekelley <zkelley@andrew.cmu.edu>
2025-12-01 21:13:58 -0500

    Added NA

--- recipeSt.py
+++ recipeSt.py
@@ -32,7 +32,8 @@ recipeDinner = {
     "Chili": ["Ground Beef", "Onion", "Crushed Tomatoes", "Crushed Tomatoes", "Kidney Beans", "Beef Broth", "Chili Seasoning"],
     "Marry Me Chicken Soup": ["Chicken", "Sun-Dried Tomatoes", "Heavy Cream", "Cream Cheese", " Tomato Paste", "Macaroni", "Chicken Broth", "Onion"],
     "French Onion Soup": ["Onion", "Beef Broth", "Provolone"],
-    "Pizza": ["Pizza"]
+    "Pizza": ["Pizza"],
+    "NA": []
 }
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
